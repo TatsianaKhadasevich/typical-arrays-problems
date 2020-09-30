55d8fc5169e848a31eb22e39b6f1221fa17f9bcf
TatsianaKhadasevich <tatsyanakhodasevich@gmail.com>
2020-09-30 17:45:08 +0300

    feat:task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array = null) {
+return  (array == null || array =='')? 0:Math.min(...array);
+  
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array = null) {
+return  (array == null || array =='')? 0:Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array = null) {
+return  (array == null || array =='')? 0: array.reduce((sum, item) => sum + item)/array.length;
 }
